df4fc70a440ca3f94bacc4790fb0f953f996274c
Federico Tempone <fedetempone5@gmail.com>
2026-03-20 20:44:18 -0300

    fix error vite deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react'
+import react from '@vitejs/plugin-react' // <-- Corregido aquí
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -13,16 +14,21 @@ export default defineConfig({
         short_name: 'BocaAccess',
         description: 'Control de ingreso vehicular para Boca Juniors',
         theme_color: '#003566',
+        background_color: '#003566',
+        display: 'standalone',
+        orientation: 'portrait',
         icons: [
           {
             src: 'logo192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'logo512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
